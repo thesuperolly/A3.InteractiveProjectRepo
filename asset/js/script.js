db8d042bfd4d3bacf828e907d58bc6fef634fa26
thesuperolly <thesuperolly@gmail.com>
2021-05-09 14:42:51 +1000

    Latest Code Iterations

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,3 @@
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-
-        var coordinates = lat + ", " + long;
-        console.log(coordinates);
-        return coordinates 
-    });
 
 
     // URL to Call JSON API data.
@@ -15,13 +6,14 @@
     
 
     // JQuery Make a new card.
-    var newCard = function(listingID, address, image, priceGuide, inspectionTimes, getDirections){
+    var newCard = function(listingID, address, image, priceGuide, inspectionTimes, getDirections, setOrder){
         $("main").append('<div id="' +
-         listingID + '" class="card"><div class="lineItem"><div class="cardLeft"><div class="cardThumbnail"><img src="' + 
+         listingID + '" class="card" style="order:' + 
+         setOrder +';"><div class="lineItem"><div class="cardLeft"><div class="cardThumbnail"><img src="' + 
          image + '" alt=""></div><div class="cardCenter"><h4>' + 
-         address + '</h4><div class="rating"><i class="fas fa-star ratings_stars"></i><i class="fas fa-star ratings_stars"></i><i class="fas fa-star ratings_stars"></i><i class="fas fa-star altColor ratings_stars"></i><i class="fas fa-star altColor ratings_stars"></i></div></div><div class="cardRight"><h5>'+ 
+         address + '</h4><div class="rating"><i class="fas fa-star ratings_stars"></i><i class="fas fa-star ratings_stars"></i><i class="fas fa-star ratings_stars"></i><i class="fas fa-star altColor ratings_stars"></i><i class="fas fa-star altColor ratings_stars"></i></div></div><div class="cardRight"><h5 class="inspectionTime">'+ 
          inspectionTimes +'</h5><div class="cardRightIcons"><div class="showInfo"><i class="fas fa-clipboard-list"></i></div><div><a href="' + 
-         getDirections +'"><i class="fas fa-directions"></i></a></div><div><i class="far fa-trash-alt"></i></div></div></div></div></div><div class="info"><div class="priceInfo"><h4>' + 
+         getDirections +'" target="_blank"><i class="fas fa-directions"></i></a></div><div><i class="far fa-trash-alt"></i></div></div></div></div></div><div class="info"><div class="priceInfo"><h4>' + 
          priceGuide + '</h4></div><div class="notesArea"><h4>My Notes:</h4><textarea placeholder="What lovely flooring" name="notes"></textarea></div></div></div>');
         }
 
@@ -31,132 +23,22 @@
 // Run document script
 $ ( document ).ready (function(){
 
+    currentLocation = getUserLocation();
+
+    setTimeout(function() {
+        getData()
+    },  3000)
+    // getData();
 
             // remove dummy cards 
             $ (".card").remove();
-
-
-            // Call to Live API Data
-            // UNCOMMENT WHEN READY=========================================================
-            var apikey = 'key_1f6eb1dfbc88eab2d49d10f31f1c7c15';
-
-            var resiSearchUrl = 'https://api.domain.com.au/v1/listings/residential/_search';
-
-            const data = {
-                "locations":
-                    [{
-                        "state": "ACT",
-                        "postCode": "2602",
-                    }]
-            };
-
-            fetch(resiSearchUrl, {
-                method: 'POST', 
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Api-Key': apikey
-                },
-                body: JSON.stringify(data),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-            //END OF UNCOMMENT SECTION====================================================== 
-
-
-
-
-
-            // Test Data
-            // COMMENT OUT WHEN GOING LIVE==================================================
-
-                // call JSON/API Info
-                // $.getJSON(jsonData, function(data){
-
-                    // look at returned data
-                    // console.log(data);
-            // END OF COMMENT OUT SECTION===================================================
-
-
-
-
-                    // loop through returned data
-                    for (let index = 0; index < data.length; index++) {
-
-                        // chack if object is a property listing
-                        if (data[index].type === "PropertyListing") {
-
-                            // create unique ID for each card.
-                            var listingID = "card no:" + index;
-
-                            // Get Litsing Attributes
-                            // Address details
-                            var getAddress = data[index].listing.propertyDetails.displayableAddress;
-
-                            // Image Thumnail
-                            var getThumbnail = data[index].listing.media[0].url;
-
-                            // Display price
-                            var getPrice = data[index].listing.priceDetails.displayPrice;
-
-                            // Inspection times
-                            var getInspection = function(){
-                                // chech if there are any inspections
-                                if (data[index].listing.inspectionSchedule.times[0]) {
-
-                                    // Shorten the inspection string to only include inspection time
-                                    let shortenInspection = function(string){
-                                        let shortString = string.slice(0, -3);
-                                        let shortestString = shortString.substr(shortString.length - 5);
-                                        return shortestString
-                                    }
-
-                                    // get opening time
-                                    let opening = data[index].listing.inspectionSchedule.times[0].openingTime;
-
-                                    // get closing time
-                                    let closing = data[index].listing.inspectionSchedule.times[0].closingTime;
-
-                                    // returne shortened inspection time
-                                    return shortenInspection(opening) + " - " + shortenInspection(closing);
-                                } else {
-                                    return "Inspections Unavailable"
-                                } 
-                            };
-
-                            // Get listing lat and lon to get directions
-                            var getLatitudeAndLongitude = {
-                                lat: data[index].listing.propertyDetails.latitude,
-                                lon: data[index].listing.propertyDetails.longitude
-                            }
-                            var getDirections = 'https://www.google.com/maps/dir/?api=1&origin=-currentLocation&destination=' + getLatitudeAndLongitude.lat + ', ' + getLatitudeAndLongitude.lon
-
-
-                            // attach new card with all created and retrieved data
-                            newCard(listingID, getAddress, getThumbnail, getPrice, getInspection(), getDirections);
-                        }
-                    // end of loop    
-                    }
-
-
-            })
-
-            // UNCOMMENT ON LIVE APP========================================================
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-            //END OF UNCOMMENT SECTION======================================================
-
-
 
 // End of document ready
 });
 
     
-
-    
 // CREATE DELETE FUNCTION
-    $(document).on('click', '.fa-trash-alt', function () {
+    $(document).on('mouseOn', '.fa-trash-alt', function () {
         $(this).closest('.card').fadeOut(300);
     });
 
@@ -185,3 +67,143 @@ $ ( document ).ready (function(){
 
 var cards = document.querySelectorAll('.card')
 console.log(cards)
+
+
+function getData() {
+    // Test Data
+
+                // Call to Live API Data
+            // UNCOMMENT WHEN READY=========================================================
+            var apikey = 'key_1f6eb1dfbc88eab2d49d10f31f1c7c15';
+
+            var resiSearchUrl = 'https://api.domain.com.au/v1/listings/residential/_search';
+
+            const data = {
+                "locations":
+                    [{
+                        "state": "ACT",
+                        "postCode": "2602",
+                    }]
+            };
+
+            fetch(resiSearchUrl, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Key': apikey
+                },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+            //END OF UNCOMMENT SECTION====================================================== 
+
+
+            // COMMENT OUT WHEN GOING LIVE==================================================
+
+                // call JSON/API Info
+                // $.getJSON(jsonData, function(data){
+
+                    // look at returned data
+                    // console.log(data);
+            // END OF COMMENT OUT SECTION===================================================
+
+                    // loop through returned data
+                    for (let index = 0; index < data.length; index++) {
+
+                        var item = data[index];
+
+                        // console.log(item);
+
+
+                        // chack if object is a property listing
+                        if (item.type === "PropertyListing" && item.listing.inspectionSchedule.times.length != 0) {
+
+                            //define variable
+                            item = item.listing;
+                            
+                            // create unique ID for each card.
+                            var listingID = "cardNo" + index;
+
+                            // Get Litsing Attributes
+                            // Address details
+                            var getAddress = item.propertyDetails.displayableAddress;
+
+                            // Image Thumnail
+                            var getThumbnail = item.media[0].url;
+
+                            // Display price
+                            var getPrice = item.priceDetails.displayPrice;
+
+                            // get opening time
+                            let opening = item.inspectionSchedule.times[0].openingTime;
+
+                            // get closing time
+                            let closing = item.inspectionSchedule.times[0].closingTime;
+
+                            // Inspection times
+                            var getInspection = function(){
+                                // chech if there are any inspections
+                                if (item.inspectionSchedule.times[0]) {
+
+                                    // Shorten the inspection string to only include inspection time
+                                    let shortenInspection = function(string){
+                                        let shortString = string.slice(0, -3);
+                                        let shortestString = shortString.substr(shortString.length - 5);
+                                        return shortestString
+                                    }
+
+                                    // returne shortened inspection time
+                                    return shortenInspection(opening) + " - " + shortenInspection(closing);
+                                } else {
+                                    return "Inspections Unavailable"
+                                } 
+                            };
+
+                            var setOrder = closing.slice(11,13)+closing.slice(14,16)
+
+                            // Get listing lat and lon to get directions
+                            var getLatitudeAndLongitude = {
+                                lat: item.propertyDetails.latitude,
+                                lon: item.propertyDetails.longitude
+                            }
+                            // Sets the URL for directions
+                            var getDirections = 'https://www.google.com/maps/dir/?api=1&origin='+currentLocation+'&destination=' + getLatitudeAndLongitude.lat + ', ' + getLatitudeAndLongitude.lon;
+
+                            console.log(getDirections);
+
+                            // attach new card with all created and retrieved data
+                            newCard(listingID, getAddress, getThumbnail, getPrice, getInspection(), getDirections, setOrder);
+                        }
+
+
+
+                    // end of loop    
+                    }
+
+
+            })
+
+            // UNCOMMENT ON LIVE APP========================================================
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+            //END OF UNCOMMENT SECTION======================================================
+
+}
+
+function getUserLocation() {
+    currentLocation = navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        var coordinates = lat + ", " + long;
+        console.log(coordinates);
+        currentLocation = coordinates;
+
+        return coordinates 
+    });  
+    
+
+}
